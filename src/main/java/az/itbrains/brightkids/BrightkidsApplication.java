c6eeb5ef845e1d86bb7930cec645f243a12941cb
GitHubUserName <emailin@example.com>
2025-12-25 20:58:02 +0400

    first commit

---
+++ src/main/java/az/itbrains/brightkids/BrightkidsApplication.java
@@ -0,0 +1,13 @@
+package az.itbrains.brightkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightkidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrightkidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/itbrains/brightkids/BrightkidsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.itbrains.brightkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightkidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
