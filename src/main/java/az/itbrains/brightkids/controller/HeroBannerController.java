6e15d2b70287c15f71b132fa48a3b223ba1db4f1
GitHubUserName <emailin@example.com>
2025-12-26 09:31:09 +0400

    HeroBanner edit

--- src/main/java/az/itbrains/brightkids/controller/HeroBannerController.java
+++ src/main/java/az/itbrains/brightkids/controller/HeroBannerController.java
@@ -10,27 +10,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/hero-banners")
 @RequiredArgsConstructor
 public class HeroBannerController {
     private final HeroBannerService heroBannerService;
 
-    @PostMapping("/hero-banners")
+    @PostMapping()
     public ResponseEntity<HeroBannerResponse> createHeroBanner(@Valid @RequestBody HeroBannerRequest request) {
         return heroBannerService.createHeroBanner(request);
     }
 
-    @GetMapping("/hero-banners/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<HeroBannerResponse> getHeroBanner(@PathVariable Long id) {
         return heroBannerService.getHeroBanner(id);
     }
 
-    @PutMapping("/hero-banners/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<HeroBannerResponse> updateHeroBanner(@Valid @RequestBody HeroBannerRequest request, @PathVariable Long id) {
         return heroBannerService.updateHeroBanner(id, request);
     }
 
-    @DeleteMapping("/hero-banners/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteHeroBanner(@PathVariable Long id) {
         heroBannerService.deleteHeroBanner(id);
         return ResponseEntity.noContent().build();
