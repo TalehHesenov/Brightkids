0325c7005cb8888dc55a0d0e05d625705e65963d
ismetaslanov2000 <ismet.aslanov2000@gmail.com>
2025-12-26 11:45:17 +0400

    PatientStory serviceimpl

--- src/main/java/az/itbrains/brightkids/models/Location.java
+++ src/main/java/az/itbrains/brightkids/models/Location.java
@@ -1,6 +1,8 @@
 package az.itbrains.brightkids.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,4 +16,8 @@ import lombok.Setter;
 public class Location {
     private String address;
     private String detail;
+
+    @ManyToOne
+    @JoinColumn(name="visit_id")
+    private Visit visit;
 }
--- src/main/java/az/itbrains/brightkids/models/Visit.java
+++ src/main/java/az/itbrains/brightkids/models/Visit.java
@@ -1,11 +1,13 @@
 package az.itbrains.brightkids.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,5 +20,7 @@ public class Visit {
     private String title;
     private String subTitle;
     private String mapUrl;
-    private List<Location> locations;
+
+    @OneToMany(mappedBy = "visit")
+    private List<Location> locations =new ArrayList<>();
 }
